060c290ef12eebd50c95932b65684ee7ca89aac6
Frank Golub <frankgolub@gmail.com>
2023-04-20 00:19:45 -0400

    Adds model

--- projects/starter-kit/src/model.py
+++ projects/starter-kit/src/model.py
@@ -1,10 +1,19 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 # define the CNN architecture
+# The architecture of the following network comports with recommendations from the CS231 and the 
+# Pytorch classification example. Batch-norm could be used as a regularizer.
+# The pattern follows: INPUT -> [[CONV -> RELU]*N -> POOL?]*M -> [FC -> RELU]*K -> FC, with the recommendation
+# that K < 3, M > 0, and pooling in force. A filter size of 5 is sufficient to avoid zero-padding and fitting to 
+# a substantial number of zero's in hidden layers that are not representative of the image.
+
+# The cifar10_cnn_exercise.ipynb was specifically relied upon.
+
 class MyModel(nn.Module):
-    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+    def __init__(self, num_classes: int = 50, dropout: float = 0.2) -> None:
 
         super().__init__()
 
@@ -14,10 +23,33 @@ class MyModel(nn.Module):
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
 
+        # convolutional layer
+        self.conv1 = nn.Conv2d(3, 8, 5)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.fc1 = nn.Linear(44944, 256*4) # 53*53*16, where 53 is close to 224/4. used error msg for first input dim
+        self.batch_norm_1 = nn.BatchNorm1d(256*4)
+        self.fc2 = nn.Linear(256*4, 128*4)
+        self.batch_norm_2 = nn.BatchNorm1d(128*4)
+        self.fc3 = nn.Linear(128*4, num_classes)  
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        x = torch.flatten(x, 1)
+
+        x = self.dropout(self.batch_norm_1(F.relu(self.fc1(x))))
+        x = self.dropout(self.batch_norm_2(F.relu(self.fc2(x))))
+        x = self.fc3(x)
         return x
 
 
